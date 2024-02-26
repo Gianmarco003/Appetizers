ff4de6f65cf722ff12056cc45a5411174c70cd2e
Gianmarco <gimmy03@icloud.com>
2024-02-26 22:30:47 +0100

    ProgressView

--- Appetizers/Views/LoadingView.swift
+++ Appetizers/Views/LoadingView.swift
@@ -7,21 +7,23 @@
 
 import SwiftUI
 
-struct ActivityIndicator: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIActivityIndicatorView {
-        let activityIndicatorView = UIActivityIndicatorView(style: .large)
-        activityIndicatorView.startAnimating()
-        return activityIndicatorView
-    }
-    func updateUIView(_ uiView: UIViewType, context: Context) {}
-}
+//struct ActivityIndicator: UIViewRepresentable {
+//    func makeUIView(context: Context) -> UIActivityIndicatorView {
+//        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+//        activityIndicatorView.startAnimating()
+//        return activityIndicatorView
+//    }
+//    func updateUIView(_ uiView: UIViewType, context: Context) {}
+//}
 
 struct LoadingView: View {
     var body: some View {
         ZStack {
             Color(.systemBackground)
                 .ignoresSafeArea()
-            ActivityIndicator()
+//            ActivityIndicator()
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
         }
     }
 }
